dfd3039c718b046012921e84fd5e4c2dcaf141b2
Daniel Earthy <danielearthy@gmail.com>
2013-11-04 22:53:17 -0800

    .

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :users
+  belongs_to :user
   belongs_to :category
-  def user
-    User.find(self.user_id).name
-  end
-  def email
-    User.find(self.user_id).email
-  end
+  
 end
